6aa5e3ba0ea68f4ec77259e1e9971d958ad3cebc
jamesying <james@taogame.com>
2017-09-03 19:37:40 +0800

    add customerMenucreate

---
+++ demo/DemoWeb/Pages/CustomerMenuCreate.cshtml.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JCSoft.WX.Framework.Api;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace DemoWeb.Pages
+{
+    public class CustomerMenuCreateModel : LoginedPageModel
+    {
+        public CustomerMenuCreateModel(IMemoryCache cache, IApiClient client) : base(cache, client)
+        {
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
