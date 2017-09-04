766a3e47479ce62aadb17c43b99252925df86a1b
JamesYinG <JamesYinG@DESKTOP-U33M66E>
2017-09-04 17:56:00 +0800

    add ace.js

--- src/JCSoft.WX.Framework.Models/Shared/ClickButton.cs
+++ src/JCSoft.WX.Framework.Models/Shared/ClickButton.cs
@@ -19,7 +19,8 @@ namespace JCSoft.WX.Framework.Models
         pic_weixin, //  弹出微信相册发图器
         location_select,     //弹出地理位置选择器
         media_id,   //下发消息（除文本消息）用户点击media_id类型按钮后，微信服务器会将开发者填写的永久素材id对应的素材下发给用户，永久素材类型可以是图片、音频、视频、图文消息。请注意：永久素材id必须是在“素材管理/新增永久素材”接口上传后获得的合法id
-        view_limited    //跳转图文消息URL用户点击view_limited类型按钮后，微信客户端将打开开发者在按钮中填写的永久素材id对应的图文消息URL，永久素材类型只支持图文消息。请注意：永久素材id必须是在“素材管理/新增永久素材”接口上传后获得的合法id
+        view_limited,    //跳转图文消息URL用户点击view_limited类型按钮后，微信客户端将打开开发者在按钮中填写的永久素材id对应的图文消息URL，永久素材类型只支持图文消息。请注意：永久素材id必须是在“素材管理/新增永久素材”接口上传后获得的合法id
+        miniprogram //小程序
     }
 
     public class ClickButton
@@ -46,5 +47,11 @@ namespace JCSoft.WX.Framework.Models
 
         [JsonProperty("news_info", NullValueHandling = NullValueHandling.Ignore)]
         public NewsInfo NewsInfo { get; set; }
+
+        [JsonProperty("appid", NullValueHandling = NullValueHandling.Ignore)]
+        public string MiniProgramAppId { get; set; }
+
+        [JsonProperty("pagePath", NullValueHandling = NullValueHandling.Ignore)]
+        public string MiniProgramPagePath { get; set; }
     }
 }
