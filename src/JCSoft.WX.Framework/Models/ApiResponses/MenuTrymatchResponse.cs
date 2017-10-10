cd9f91862b9ca0a65a3451652890fb831a521713
JamesYinG <JamesYinG@DESKTOP-U33M66E>
2017-10-10 14:14:28 +0800

    1.add menu delcontional 2.method string to enum

---
+++ src/JCSoft.WX.Framework/Models/ApiResponses/MenuTrymatchResponse.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JCSoft.WX.Framework.Models.ApiResponses
+{
+    public class MenuTrymatchResponse : ApiResponse
+    {
+        [JsonProperty("button")]
+        public List<ClickButton> Buttons { get; set; }
+    }
+}
