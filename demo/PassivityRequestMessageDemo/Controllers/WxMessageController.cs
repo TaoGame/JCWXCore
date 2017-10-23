ad77671ca6b5cd0301ede4a396f4c7fcad355919
jamesying <jamesying@DESKTOP-04V6SGQ>
2017-10-23 14:26:32 +0800

    add log to demo

--- demo/PassivityRequestMessageDemo/Controllers/WxMessageController.cs
+++ demo/PassivityRequestMessageDemo/Controllers/WxMessageController.cs
@@ -3,7 +3,9 @@ using JCSoft.WX.Framework.Models;
 using JCSoft.WX.Framework.Models.Requests;
 using JCSoft.WX.Framework.Models.Responses;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,9 +17,11 @@ namespace PassivityRequestMessageDemo.Controllers
     public class WxMessageController : Controller
     {
         private WXOptions wXOptions;
-        public WxMessageController(IOptions<WXOptions> options)
+        private ILogger<WxMessageController> _logger;
+        public WxMessageController(IOptions<WXOptions> options, ILoggerFactory loggerFactory)
         {
             wXOptions = options.Value;
+            _logger = loggerFactory.CreateLogger<WxMessageController>();
         }
 
         [HttpGet]
@@ -30,13 +34,19 @@ namespace PassivityRequestMessageDemo.Controllers
         public ResponseMessage Post([FromBody]RequestMessage request)
         {
             var textRequest = request as RequestTextMessage;
-            return new ResponseTextMessage
+            var response = new ResponseTextMessage
             {
                 FromUserName = "jamesying",
                 ToUserName = "candy",
                 Content = textRequest?.Content ?? "this is a test!",
                 CreateTime = DateTime.Now.Ticks,
             };
+
+            _logger.LogInformation("[Recive a Message]\r\n request is :{0} \r\n response:{1}", 
+                JsonConvert.SerializeObject(request), 
+                JsonConvert.SerializeObject(response));
+
+            return response;
         }
 
         [HttpPut]
