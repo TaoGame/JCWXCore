1fb99d6b36375771e7a2cb7b28f16532b19f790c
jamesying <jamesying@DESKTOP-04V6SGQ>
2017-10-23 14:26:32 +0800

    add log to demo

--- demo/PassivityRequestMessageDemo/Startup.cs
+++ demo/PassivityRequestMessageDemo/Startup.cs
@@ -50,8 +50,11 @@ namespace PassivityRequestMessageDemo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            loggerFactory.AddApplicationInsights(app.ApplicationServices, LogLevel.Trace);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
