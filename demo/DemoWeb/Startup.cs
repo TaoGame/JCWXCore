4e1ba37e9bdf2e34b360435da4ff6e912847e89e
应晔 <yingye001@huazhu.com>
2018-06-11 15:22:41 +0800

    add nuget package

--- demo/DemoWeb/Startup.cs
+++ demo/DemoWeb/Startup.cs
@@ -1,16 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using JCSoft.Core.Net.Http;
-using Microsoft.AspNetCore.Mvc;
-using JCSoft.WX.Mvc.Formatters;
-using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
-using DemoWeb.Codes;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace DemoWeb
