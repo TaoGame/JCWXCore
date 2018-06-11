38e762e1cf6489d4fe1f4be2cb3340a7c7baea4c
应晔 <yingye001@huazhu.com>
2018-06-11 15:47:43 +0800

    add valide

---
+++ demo/DemoWeb/Controllers/WXValidController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DemoWeb.Controllers
+{
+    public class WXValidController : Controller
+    {
+        private readonly ILogger<WXValidController> _logger;
+        public WXValidController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<WXValidController>();
+        }
+
+        public string Index(string echostr, string signature, string timestamp)
+        {
+            _logger.LogInformation($"echostr:{echostr}, signature:{signature}, timestamp:{timestamp}");
+            return echostr;
+        }
+    }
+}
