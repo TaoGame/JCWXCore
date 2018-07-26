bd45f2423ea2dc57477368cbf1be762f83d6b4de
jamesying <james@taogame.com>
2018-07-26 11:06:57 +0800

    小程序：小程序使用的登录凭证校验

---
+++ src/JCSoft.WX.Framework/Models/Miniapp/Requests/SnsJscodeToSessionRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using JCSoft.Core.Net.Http;
+using JCSoft.WX.Framework.Models.ApiRequests;
+using JCSoft.WX.Framework.Models.Miniapp.Respones;
+
+namespace JCSoft.WX.Framework.Models.Miniapp.Requests
+{
+    /// <summary>
+    /// 小程序使用的登录凭证校验
+    /// </summary>
+    public class SnsJscodeToSessionRequest : ApiRequest<SnsJscodeToSessionResponse>
+    {
+        /// <summary>
+        /// 小程序appid
+        /// </summary>
+        public string AppId { get; set; }
+        /// <summary>
+        /// 小程序密钥
+        /// </summary>
+        public string AppSecret { get; set; }
+        /// <summary>
+        /// 通过wx.login获取的code
+        /// </summary>
+        public string Code { get; set; }    
+        internal override HttpRequestActionType Method { get; } = HttpRequestActionType.Content;
+        protected override string UrlFormat { get; } = "https://api.weixin.qq.com/sns/jscode2session?appid={0}&secret={1}&js_code={2}&grant_type=authorization_code";
+        internal override string GetUrl()
+        {
+             return String.Format(UrlFormat, AppId, AppSecret, Code );
+        }
+
+        protected override bool NeedToken { get; } = false;
+        internal override string GetPostContent()
+        {
+            return String.Empty;
+        }
+    }
+}
---
+++ src/JCSoft.WX.Framework/Models/Miniapp/Respones/SnsJscodeToSessionResponse.cs
@@ -0,0 +1,15 @@
+﻿using JCSoft.WX.Framework.Models.ApiResponses;
+using Newtonsoft.Json;
+
+namespace JCSoft.WX.Framework.Models.Miniapp.Respones
+{
+    public class SnsJscodeToSessionResponse : ApiResponse
+    {
+        [JsonProperty("openid")]
+        public string OpenId { get; set; }
+        [JsonProperty("session_key")]
+        public string SessionKey { get; set; }
+        [JsonProperty("unionid")]
+        public string Unionid { get; set; }
+    }
+}
